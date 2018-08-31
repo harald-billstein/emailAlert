09caf481daa2fc835dd72cece26b8a69ec9ba0c3
Harald Billstein <harald.billstein@gmail.com>
2018-08-31 15:34:43 +0200

    init

---
+++ src/main/java/com/woodtailer/emailalertplugin/EmailalertpluginApplication.java
@@ -0,0 +1,15 @@
+package com.woodtailer.emailalertplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailalertpluginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmailalertpluginApplication.class, args);
+
+  }
+
+
+}
